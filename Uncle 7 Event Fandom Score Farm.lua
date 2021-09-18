82e599237e7ccb8d8a5e7f01ca776d005da0e2ea
Vultiee <80932924+Vultiee@users.noreply.github.com>
2021-09-18 17:52:09 -0400

    Script

---
+++ Uncle 7 Event Fandom Score Farm.lua
@@ -0,0 +1,11 @@
+local function fub()
+    while true do
+        wait(0.001)
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(268, 4, 17)
+        wait(0.001)
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(277.038025, 230.387604, -7.11249638, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+    end
+end
+fub()
+
+--Game link: https://www.roblox.com/games/7277488595/Launch-Party
